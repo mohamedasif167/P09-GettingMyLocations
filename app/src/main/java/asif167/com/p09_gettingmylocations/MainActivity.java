144b4edfa38a3de81e7f630d2d1a989fbdba21f2
mohamedasif167 <mohamedasif167@gmail.com>
2017-07-18 14:51:07 +0800

    Problem Statement

--- app/src/main/java/asif167/com/p09_gettingmylocations/MainActivity.java
+++ app/src/main/java/asif167/com/p09_gettingmylocations/MainActivity.java
@@ -1,17 +1,42 @@
 package asif167.com.p09_gettingmylocations;
 
+import android.content.Intent;
+import android.location.Location;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.PermissionChecker;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 
 import org.w3c.dom.Text;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+public class MainActivity extends AppCompatActivity implements
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,LocationListener {
 
     Button btnStart,btnStop,btnCheck;
     TextView lat,lng;
 
+    private GoogleApiClient mGoogleApiClient;
+    private Location mLocation;
+    String folderLocation;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,11 +48,148 @@ public class MainActivity extends AppCompatActivity {
         lat = (TextView) findViewById(R.id.lat);
         lng = (TextView) findViewById(R.id.lng);
 
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
 
 
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, DetectorService.class);
+                startService(i);
+            }
+        });
+
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, DetectorService.class);
+                stopService(i);
+            }
+        });
+
+        btnCheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                folderLocation = Environment.getExternalStorageDirectory().getAbsolutePath() + "/Test";
+                File targetFile = new File(folderLocation, "locationNew.txt");
+
+                if (targetFile.exists() == true){
+                    String data ="";
+                    try {
+                        FileReader reader = new FileReader(targetFile);
+                        BufferedReader br = new BufferedReader(reader);
+                        String line = br.readLine();
+                        while (line != null){
+                            data += line + "\n";
+                            line = br.readLine();
+                        }
+                        br.close();
+                        reader.close();
+                    } catch (Exception e) {
+                        Toast.makeText(MainActivity.this, "Failed to read!",
+                                Toast.LENGTH_LONG).show();
+                        e.printStackTrace();
+                    }
+                    Toast.makeText(MainActivity.this, data, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+        int permissionCheck_Coarse = ContextCompat.checkSelfPermission(
+                MainActivity.this, android.Manifest.permission.ACCESS_COARSE_LOCATION);
+        int permissionCheck_Fine = ContextCompat.checkSelfPermission(
+                MainActivity.this, android.Manifest.permission.ACCESS_FINE_LOCATION);
+
+        if (permissionCheck_Coarse == PermissionChecker.PERMISSION_GRANTED
+                ||  permissionCheck_Fine  == PermissionChecker.PERMISSION_GRANTED){
+
+            mLocation = LocationServices.FusedLocationApi.getLastLocation(
+                    mGoogleApiClient);
+
+            LocationRequest mLocationRequest = LocationRequest.create();
+            mLocationRequest.setPriority(LocationRequest
+                    .PRIORITY_BALANCED_POWER_ACCURACY);
+            mLocationRequest.setInterval(10000);
+            mLocationRequest.setFastestInterval(5000);
+            mLocationRequest.setSmallestDisplacement(100);
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
 
 
+        } else {
+            mLocation = null;
+            Toast.makeText(MainActivity.this,
+                    "Permission not granted to retrieve location info",
+                    Toast.LENGTH_SHORT).show();
+        }
+
+        if (mLocation != null) {
+//            Toast.makeText(this, "Lat : " + mLocation.getLatitude() +
+//                            " Lng : " + mLocation.getLongitude(),
+//                    Toast.LENGTH_SHORT).show();
+
+            lat.setText(mLocation.getLatitude()+"");
+            lng.setText(mLocation.getLongitude()+"");
+
+        } else {
+            Toast.makeText(this, "Location not Detected",
+                    Toast.LENGTH_SHORT).show();
+        }
 
 
     }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        //the detected location is given by the variable location in the signature
+
+        folderLocation = Environment.getExternalStorageDirectory().getAbsolutePath() + "/Test";
+        //Write Into File Here
+        File targetFile = new File(folderLocation, "locationNew.txt");
+
+        try {
+            FileWriter writer = new FileWriter(targetFile, true);
+            writer.write(location.getLatitude()+","+location.getLongitude()+"\n");
+            writer.flush();
+            writer.close();
+            //Toast.makeText(getApplicationContext(), "Success", Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "Failed to write!",
+                    Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        }
+
+    }
+
+
+    @Override
+    public void onConnectionSuspended(int i) { }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) { }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Connect to Play Services
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mGoogleApiClient.isConnected()) {
+            //Disconnects from play services when the activity stops
+            mGoogleApiClient.disconnect();
+        }
+    }
+
 }
